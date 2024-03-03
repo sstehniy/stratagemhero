b8cab1eb06b825f2cc98239a53baf37e74a58ce8
Stanislav Stehniy <sstehniy@gmail.com>
2024-03-04 00:34:15 +0100

    Add links and disclaimer to App component

--- src/App.tsx
+++ src/App.tsx
@@ -296,6 +296,47 @@ function App() {
 
   return (
     <div className="main-container">
+      <div
+        style={{
+          position: "fixed",
+          bottom: 5,
+          left: 5,
+          fontSize: 12,
+          color: "var(--lightgrey)",
+          width: 400,
+        }}
+      >
+        <p>
+          Check out Helldivers 2 on{" "}
+          <a
+            href="https://store.steampowered.com/app/553850/HELLDIVERS_2/"
+            target="_blank"
+          >
+            Steam
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://www.playstation.com/en-us/games/helldivers-2/"
+            target="_blank"
+          >
+            Playstation
+          </a>
+          !<br />
+        </p>
+        Report an issue{" "}
+        <a
+          href="https://github.com/sstehniy/stratagemhero/issues"
+          target="_blank"
+        >
+          here
+        </a>
+        <br />
+        Not affiliated with Helldivers, Arrowhead Studios, or Playstation
+        Studios.
+        <br />
+        This project is just a fan-made game to practice skills for spreading
+        democrary across the universe. Not for profit!!!
+      </div>
       <div style={{ position: "fixed", top: 10, right: 10 }}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
