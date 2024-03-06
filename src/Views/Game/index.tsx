7a01e838387fa34f62c20cb2da12658fbeb633fe
Stanislav Stehniy <sstehniy@gmail.com>
2024-03-06 02:08:42 +0100

    Refactor retry button styling and position

--- src/Views/Game/index.tsx
+++ src/Views/Game/index.tsx
@@ -150,27 +150,21 @@ export const Game = ({
           }}
         ></div>
       </div>
-      <div>
-        <button
-          className={styles.retryButton}
-          tabIndex={1}
-          onClick={handleRetry}
+      <button className={styles.retryButton} tabIndex={1} onClick={handleRetry}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="1em"
+          height="1em"
+          viewBox="0 0 16 16"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="1em"
-            height="1em"
-            viewBox="0 0 16 16"
-          >
-            <path
-              fill="currentColor"
-              fillRule="evenodd"
-              d="M7.32.029a8 8 0 0 1 7.18 3.307V1.75a.75.75 0 0 1 1.5 0V6h-4.25a.75.75 0 0 1 0-1.5h1.727A6.5 6.5 0 0 0 1.694 6.424A.75.75 0 1 1 .239 6.06A8 8 0 0 1 7.319.03Zm-3.4 14.852A8 8 0 0 0 15.76 9.94a.75.75 0 0 0-1.455-.364A6.5 6.5 0 0 1 2.523 11.5H4.25a.75.75 0 0 0 0-1.5H0v4.25a.75.75 0 0 0 1.5 0v-1.586a8 8 0 0 0 2.42 2.217"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-        </button>
-      </div>
+          <path
+            fill="currentColor"
+            fillRule="evenodd"
+            d="M7.32.029a8 8 0 0 1 7.18 3.307V1.75a.75.75 0 0 1 1.5 0V6h-4.25a.75.75 0 0 1 0-1.5h1.727A6.5 6.5 0 0 0 1.694 6.424A.75.75 0 1 1 .239 6.06A8 8 0 0 1 7.319.03Zm-3.4 14.852A8 8 0 0 0 15.76 9.94a.75.75 0 0 0-1.455-.364A6.5 6.5 0 0 1 2.523 11.5H4.25a.75.75 0 0 0 0-1.5H0v4.25a.75.75 0 0 0 1.5 0v-1.586a8 8 0 0 0 2.42 2.217"
+            clipRule="evenodd"
+          ></path>
+        </svg>
+      </button>
     </div>
   );
 };
